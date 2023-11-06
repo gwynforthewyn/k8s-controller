bdb6df8cccc7e232a6f96ac4f04a5f1723aac04e
Gwyn <gwyn@playtechnique.io>
2023-11-06 07:12:27 -0700

    Starting to add a reconciler. Committing now to perform an experiment on another branch.

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"time"
 )
@@ -15,15 +18,17 @@ import "sigs.k8s.io/controller-runtime/pkg/reconcile"
 // >>> "hello, world"
 
 type HelloReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
 }
 
+// The reconcile method will contain the logic to see whether a playtechnique/labelme label exists and add
+// a playtechnique/labelled label.
 func (r *HelloReconciler) Reconcile(ctx context.Context, request reconcile.Request) (reconcile.Result, error) {
 	fmt.Println("Hello, world!")
 
 	return reconcile.Result{}, nil
 }
-
-//type hello struct{}
 
 func main() {
 	cfg := config.GetConfigOrDie()
@@ -34,8 +39,15 @@ func main() {
 		panic(err)
 	}
 
-	//builder := controllerruntime.NewControllerManagedBy(ctrlManager)
-	//err = builder.For(&hello{}).Complete(&HelloReconciler{})
+	reconciler := HelloReconciler{
+		Client: ctrlManager.GetClient(),
+		Scheme: ctrlManager.GetScheme(),
+	}
+
+	//What does the controller do here?
+	ctrl, err := controller.New("gwyn-controller", ctrlManager, controller.Options{
+		Reconciler: &reconciler,
+	})
 
 	if err != nil {
 		panic(err)
